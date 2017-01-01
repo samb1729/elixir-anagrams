15366949610a9cf77fc42fe74f216ff88f34813d
Sam Broughton <sambroughton@hotmail.co.uk>
2017-01-01 04:28:27 +0000

    Filter words with non-alphabetical characters

--- lib/anagrams/file.ex
+++ lib/anagrams/file.ex
@@ -5,10 +5,16 @@ defmodule Anagrams.File do
     open_file(file)
     |> Stream.map(&String.trim/1)
     |> Stream.map(&String.downcase/1)
+    |> filter_non_alphabetical
     |> Enum.to_list
   end
 
   def open_file(file) do
     File.stream!(file)
+  end
+
+  defp filter_non_alphabetical(words) do
+    words
+    |> Enum.filter(&(String.match? &1, ~r{^[a-z]+$}))
   end
 end
