3053c2fb7448c2a6703005b4b292d727d71dd7d9
Sam Broughton <sambroughton@hotmail.co.uk>
2017-01-08 19:00:33 +0000

    Use ExCoveralls for code coverage

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule Anagrams.Mixfile do
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     deps: deps(),
+     test_coverage: [tool: ExCoveralls]
+    ]
   end
 
   # Configuration for the OTP application
@@ -18,16 +20,7 @@ defmodule Anagrams.Mixfile do
      mod: {Anagrams, []}]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:excoveralls, "~> 0.5", only: :test}]
   end
 end
